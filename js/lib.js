9f3e87d785114b679c04b44483a495b11bd49d5b
trong <trong>
2026-06-04 10:12:39 +0700

    js

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,27 @@
-/*khoi tao  js*/
+function printBCC(num)
+{
+    let result = "";
+    let i = 1;
+    while(i<=10)
+    {
+        result += `${num} x ${i} = ${num*i} <br>`; 
+        i++;
+    }
+    document.getElementById("result").innerHTML = result;
+}
+
+function tinhtien()
+{
+    const soluong = document.getElementsByClassName("soluong");
+    const dongia = document.getElementsByClassName("dongia");
+    const thanhtien = document.getElementsByClassName("thanhtien");
+    let i = 0, thanhtoan=0; 
+    while(i<soluong.length)
+    {
+        thanhtien[i].innerHTML=Number(dongia[i].innerHTML)*Number(soluong[i].innerHTML); 
+        thanhtoan+=Number(thanhtien[i].innerHTML);
+        i++;
+    }
+    document.getElementById("thanhtoan").innerHTML = thanhtoan;
+}
+tinhtien();
