7009302242cb667faab0a136bc00d4a11ea90d9f
Your Name <you@example.com>
2026-06-18 10:14:29 +0700

    update

--- js/lib.js
+++ js/lib.js
@@ -24,38 +24,113 @@ function tinhtien()
     }
     document.getElementById("thanhtoan").innerHTML = thanhtoan;
 }
-tinhtien();
 
+const products = [
+    {
+        name: "Phở Bò Hà Nội",
+        price: "45.000đ",
+        image: "../assets/images/pho-bo.jpg",
+        info: "Nước dùng đậm đà, bánh phở mềm, thịt bò tươi ngon",
+        link: "../html/pho-bo.html"
+    },
+    {
+        name: "Bún Chả Hà Nội",
+        price: "50.000đ",
+        image: "../assets/images/bun-cha.jfif",
+        info: "Chả nướng thơm lừng, ăn kèm bún và rau sống",
+        link: "../html/pho-bo.html"
+    },
+    {
+        name: "Cơm Tấm Sườn",
+        price: "55.000đ",
+        image: "../assets/images/com-tam.jpg",
+        info: "Sườn nướng đậm vị, trứng ốp la và bì heo",
+        link: "../html/pho-bo.html"
+    },
+    {
+        name: "Bánh Mì Thịt Nướng",
+        price: "25.000đ",
+        image: "../assets/images/banh-mi.jpg",
+        info: "Bánh mì giòn rụm, nhân thịt nướng và rau củ",
+        link: "#"
+    },
+    {
+        name: "Bún Bò Huế",
+        price: "50.000đ",
+        image: "../assets/images/bun-bo-hue.jpg",
+        info: "Hương vị cay nồng đặc trưng, thịt bò mềm",
+        link: "#"
+    },
+    {
+        name: "Mì Quảng",
+        price: "45.000đ",
+        image: "../assets/images/mi-quang.jpg",
+        info: "Sợi mì vàng, nước dùng đậm đà, topping đa dạng",
+        link: "#"
+    },
+    {
+        name: "Gỏi Cuốn Tôm Thịt",
+        price: "30.000đ",
+        image: "../assets/images/goi-cuon.jpg",
+        info: "Cuốn tươi với tôm, thịt và rau sống",
+        link: "#"
+    },
+    {
+        name: "Chả Cá Lã Vọng",
+        price: "80.000đ",
+        image: "../assets/images/cha-ca.jpg",
+        info: "Cá tẩm ướp nghệ, ăn cùng bún và thì là",
+        link: "#"
+    },
+    {
+        name: "Hủ Tiếu Nam Vang",
+        price: "50.000đ",
+        image: "../assets/images/hu-tieu.jpg",
+        info: "Nước dùng ngọt thanh, topping phong phú",
+        link: "#"
+    },
+    {
+        name: "Bánh Xèo Miền Tây",
+        price: "40.000đ",
+        image: "../assets/images/banh-xeo.jpg",
+        info: "Vỏ bánh giòn, nhân tôm thịt, ăn kèm rau sống",
+        link: "#"
+    }
+    ];
 
-function create(obj){
-    const item=document.createElement("div");
-    item.setAttribute("class","card product-item");
-    const img=document.createElement("div");
-    img.setAttribute("class","ratio ratio-1x1 product-img");
-    const info=document.createElement("div");
-    info.setAttribute("class","card-body product-info");
+    function createItem(obj) {
+        const productList = document.getElementById("product-list")
+        productList.innerHTML += `
+        <div class ="col product-item">
+                    <div class="card ">
+                        <div class=" ratio ratio-1x1 product-image">
+                            <img src= "${obj.image}" alt="${obj.name}" class="card-img-top object-fit-cover">
+                        </div>
+                        <!-- info product -->
+                        <div class="card-body product-info">
+                            <h4 class="card title">${obj.name}</h4>
+                            <h6 class="text-danger">${obj.price}</h6>
+                            <p>${obj.info}</p>
+                            <a href="${obj.link}" class="btn btn-primary">Còn hàng</a>
+                        </div>
+                    </div>
+        </div>`;
+    }
 
-    item.appendChild(card);
-    item.innerHTML+=`
-        <div class ="ratio ratio-1x1 product-img" >
-            <img class="image" src="${obj.image}" alt="Banh mi" style="object-fit:cover;" >
-        </div>
-        <div class ="card-body product-info">
-            <h4 class="name">${obj.name}</h4>
-            <h5 class="price">Gia: ${obj.price}</h5>
-            <p class="description">Gom: ${obj.description}</p>
-            <a class="link"  href="#">Xem chi tiet san pham</a>
-        </div>
-    `;
-    document.getElementById("product-list").appendChild(item);
-}
+    function AllItem (objArray) {
+        let i = 0;
+        while (i<objArray.length) {
+            createItem(objArray[i]);
+            i++;
+        }
+} 
 
 
 /* 
 <div class ="row row-cols-1 row-cols-md-4 g-4 product-list">
     <div class="card product-item">
         <div class ="ratio ratio-1x1 product-img" >
-            <img class="image" src="../assets/image/banhmi.jfif" alt="Banh mi" style="object-fit:cover;" >
+            <img class="image" src="../assets/images/banhmi.jfif" alt="Banh mi" style="object-fit:cover;" >
         </div>
         <div class ="card-body product-info">
             <h4 class="name">Banh mi</h4>
